21177b82c420286208985e420fda0cd4bac94caf
kushal <mohitsinghkpsc@gmail.com>
2023-12-29 22:12:03 +0530

    complete sign up page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
-import SignOut from './pages/SignOut';
+import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
@@ -14,7 +14,7 @@ export default function App() {
         <Routes>
           <Route path = "/" element={<Home/>}/>
           <Route path = "/sign-in" element={<SignIn/>}/>
-          <Route path = "/sign-out" element={<SignOut/>}/>
+          <Route path = "/sign-up" element={<SignUp/>}/>
           <Route path = "/about" element={<About/>}/>
           <Route path = "/profile" element={<Profile/>}/>
         </Routes>
