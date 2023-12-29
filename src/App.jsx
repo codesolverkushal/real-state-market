bc9c588ed7eb1385ce60bbd566ef098578378a60
kushal <mohitsinghkpsc@gmail.com>
2023-12-29 10:08:05 +0530

    router given to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
-    <div className=''>App</div>
+     <BrowserRouter>
+        <Routes>
+          <Route path = "/" element={<Home/>}/>
+          <Route path = "/sign-in" element={<SignIn/>}/>
+          <Route path = "/sign-out" element={<SignOut/>}/>
+          <Route path = "/about" element={<About/>}/>
+          <Route path = "/profile" element={<Profile/>}/>
+        </Routes>
+     </BrowserRouter>
   )
 }
